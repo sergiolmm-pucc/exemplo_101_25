1296d8b41139fc352d281cb8da6045827011c3c9
Sergio Marques <sergio.marques@puc-campinas.edu.br>
2025-05-15 09:04:34 -0300

    aula de teste e actions 2

---
+++ sum.js
@@ -0,0 +1,10 @@
+
+
+function sum(a,b) {
+	//var c = 10;
+	
+	return a + b;
+}
+
+module.exports = sum;
+
---
+++ sum.test.js
@@ -0,0 +1,11 @@
+
+
+const sum = require('./sum');
+
+
+test('Soma de dois numerors  1 + 2 = 3 ', () => {
+
+  expect(sum(1,2)).toBe(3); 
+ 
+});
+
